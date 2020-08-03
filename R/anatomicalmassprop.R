46e43b2f5fb84f7de47e98583fa0f17b51c3e3dd
charvey23 <harveyca@umich.edu>
2020-08-03 16:32:47 -0400

    incorporated all basic moment of inertia functions

---
+++ R/anatomicalmassprop.R
@@ -0,0 +1,75 @@
+
+
+
+#' Title
+#'
+#' @param m full mass of the bone
+#' @param l full length of the bone
+#' @param r_out outer radius of bone
+#' @param r_in inner radius of bone
+#' @param rho density of the bone (kg/m^3)
+#' @param start 3D point where bone starts. This point must be defined relative to the vehicle reference point (VRP) in the VRP frame of reference
+#' @param end 3D point where bone ends. This point must be defined relative to the vehicle reference point (VRP) in the VRP frame of reference
+#'
+#' @return
+#'
+#' @section Warning:
+#' the parallel axis theorem only works if the moment of inertia is calculated about components centroidal axis
+#'
+#' @export
+#'
+#' @examples
+massprop_bones <- function(m,l,r_out,r_in,rho,start,end){
+  # ---------------------------------------------------------------------------------
+  # ---------------------------- Define geometry ------------------------------------
+  # ---------------------------------------------------------------------------------
+  vol   = m/rho # total volume of the bone
+
+  # calculate how thick the cap needs to be to match the vol1 = m/rho and vol2 = geometric
+  cap_t = (-(0.5*l*((r_out^2)-(r_in^2)))/(r_in^2))+(vol/(2*pi*(r_in^2)))
+  cap_m = pi*cap_t*cap_r^2
+
+  cyl_m = m - (2*cap_m)
+  cyl_l = l - (2*cap_t)
+
+  # ---------------------------------------------------------------------------------
+  # ------------------------------- Adjust axis -------------------------------------
+  # ---------------------------------------------------------------------------------
+  z_axis = bone_end-bone_start;
+  temp_vec = c(1,1,1) # arbitrary
+  x_axis = cross(z_axis_b,temp_vec/Norm(temp_vec));
+
+  # doesn't matter where the x axis points as long as:
+  # 1. we know what it is 2. it's orthogonal to z
+  VRP2object = calc_rot(z_axis,x_axis);
+
+  # ---------------------------------------------------------------------------------
+  # ----------------------- Calculate moment of inertia -----------------------------
+  # ---------------------------------------------------------------------------------
+
+  # --- Calculate the moment of inertia about the CG of the individual components --
+  # Frame of reference: Bone | Origin: Bone CG
+  I_cyl_b = calc_inertia_cylhollow(r_out, r_in, cyl_l, cyl_m)
+  I_cap_b = calc_inertia_cylsolid(r_out, cap_t, cap_m)
+
+  # want to move origin from CG to VRP but need to know where the bone is relative to the VRP origin
+  off_bone = VRP2object %*% bone_start  # Frame of reference: Bone | Origin: VRP
+
+  # determine the offset vector for each component with Frame of reference: Bone | Origin: VRP
+  I_cyl_off   = c(0,0,0.5*l_bone) + off_bone            # the hollow cylinder is displaced halfway along the bone (in z-axis)
+  I_cap1_off  = c(0,0,0.5*cap_t) + off_bone             # Cap 1 edge centered on the beginning of the bone
+  I_cap2_off  = c(0,0,(l_bone - (0.5*cap_t)))+ off_bone # Cap 2 edge centered on the end of the bone
+
+  # need to adjust the moment of inertia tensor - Frame of reference: Bone | Origin: VRP
+  I_cyl_vrp   = parallelaxis(I_cyl,-I_cyl_off,m_cyl)
+  I_cap1_vrp  = parallelaxis(I_cap,-I_cap1_off,m_cap)
+  I_cap2_vrp  = parallelaxis(I_cap,-I_cap2_off,m_cap)
+
+  I_boneaxis  = I_cyl_vrp + I_cap1_vrp + I_cap2_vrp      # Frame of reference: Bone | Origin: VRP
+
+  mass_prop = list()
+  mass_prop$I  = t(VRP2object) %*% bone_start %*% VRP2object # Frame of reference: VRP | Origin: VRP
+  mass_prop$CG = 0.5*(bone_start + bone_end) # Frame of reference: VRP | Origin: VRP
+
+  return(mass_prop)
+}
--- R/mathematicalfunctions.R
+++ R/mathematicalfunctions.R
@@ -129,4 +129,207 @@ parallelaxis <- function(I_CG, offset_vec, m){
 }
 
 
+
+# ----------------------------------------------------------------------------------------
+# ------------------------- Moment of interia tensors Properties  ------------------------
+# ----------------------------------------------------------------------------------------
+
 # ---------------------- Mass Properties - Solid cylinder -------------------------------
+#' Moment of inertia tensor of a solid cylinder
+#'
+#' @param r radius of the cylinder
+#' @param h height of the cylinder
+#' @param m mass of the cylinder
+#'
+#' @author Christina Harvey
+#'
+#' @return Function returns the moment of inertia tensor of a solid cylinder about
+#' its center of gravity with z oriented through it's major axis
+#'
+#'
+#' @examples
+#'
+#' @export
+#'
+calc_inertia_cylsolid <- function(r, h, m){
+
+  I = matrix(0, nrow = 3, ncol = 3)
+  I[1,1] = (1/12)*(3*r^2+h^2) # Ixx
+  I[2,2] = (1/12)*(3*r^2+h^2) # Iyy
+  I[3,3] = (0.5)*(r^2)        # Izz
+  I = m*I
+
+  return(I)
+}
+
+# ---------------------- Mass Properties - Hollow cylinder -------------------------------
+#' Moment of inertia tensor of a solid cylinder
+#'
+#' @param r_out outer radius of the cylinder
+#' @param r_in inner radius of the cylinder
+#' @param h height of the cylinder
+#' @param m mass of the cylinder
+#'
+#' @author Christina Harvey
+#'
+#' @return Function returns the moment of inertia tensor of a hollow cylinder about
+#' its center of gravity with z oriented through it's major axis
+#'
+#'
+#' @examples
+#'
+#' @export
+#'
+calc_inertia_cylhollow <- function(r_out, r_in, h, m){
+
+  temp_r = r_out^2 + r_in^2
+
+  I = matrix(0, nrow = 3, ncol = 3)
+  I[1,1] = (1/12)*(3*temp_r+h^2) # Ixx
+  I[2,2] = (1/12)*(3*temp_r+h^2) # Iyy
+  I[3,3] = (0.5)*(temp_r)        # Izz
+  I = m*I
+
+  return(I)
+}
+
+# -------------------- Mass Properties - solid square pyramid  -------------------------------
+#' Moment of inertia tensor of a solid square pyramid
+#'
+#' @param w full width of one side of the pyramid base
+#' @param h height of the pyramid
+#' @param m mass of the pyramid
+#'
+#' @author Christina Harvey
+#'
+#' @return Function returns the moment of inertia tensor of a solid square pyramid cylinder about
+#' its center of gravity with z oriented through it's major axis
+#'
+#'
+#' @examples
+#'
+#' @export
+#'
+calc_inertia_pyrasolid <- function(w, h, m){
+
+  temp_r = r_out^2 + r_in^2
+
+  I = matrix(0, nrow = 3, ncol = 3)
+  I[1,1] = (w^2+0.5*h^2) # Ixx
+  I[2,2] = (w^2+0.5*h^2) # Iyy
+  I[3,3] = 2*(w^2)       # Izz
+  I = (1/5)*m*I
+
+  return(I)
+}
+
+# -------------------- Mass Properties - flat rectangular plate  -------------------------------
+#' Moment of inertia tensor of a solid square pyramid
+#'
+#' @param w full width of one side of the pyramid base
+#' @param h height of the pyramid
+#' @param m mass of the pyramid
+#'
+#' @author Christina Harvey
+#'
+#' @return Function returns the moment of inertia tensor of a flat plate about
+#' its center of gravity with z oriented parallel with it's long edge
+#'
+#'
+#' @examples
+#'
+#' @export
+#'
+calc_inertia_platerect <- function(w, h, m){
+
+  temp_r = r_out^2 + r_in^2
+
+  I = matrix(0, nrow = 3, ncol = 3)
+  I[1,1] = (w^2+h^2) # Ixx
+  I[2,2] = (h^2)     # Iyy
+  I[3,3] = (w^2)     # Izz
+  I = (1/12)*m*I
+
+  return(I)
+}
+
+
+# -------------------- Mass Properties - flat triangular plate  -------------------------------
+#' Moment of inertia tensor of a solid square pyramid
+#'
+#' @param pts a matrix of the three 3D points that define a point on the triangular plate;
+#' each point should be a different row as follows:
+#' pt1x, pt1y, pt1z
+#' pt2x, pt1y, pt2z
+#' pt3x, pt3y, pt3z
+#'
+#' @param a  area of the triangular plate
+#' @param rho density of the material
+#' @param t  thickness of the plate
+#' @param desired_prop either "I" or "CG" depending on the desired output
+#'
+#' @author Christina Harvey
+#'
+#' @return Function returns the moment of inertia tensor of a flat triangular plate about
+#' its center of gravity z axis defined by the input pts
+#'
+#' @section Warning:
+#' The input points should be defined in a counterclockwise direction around the plate
+#' in the triangular plate frame of reference
+#' i.e. all z components should be equal
+#'
+#'
+#' @examples
+#'
+#' @export
+#'
+calc_inertia_platetri <- function(pts, a, rho, t, desired_prop){
+
+  pts = rbind(pts,pts[1,]) # need to add the first point to allow circular calculation
+
+  # calculate the center of gravity of the plate
+  x1  = 0
+  y1  = 0
+  for (i in 1:3){
+    x1   = x1 + (1/(6*a))*(((pts[i,1]*pts[i+1,2])-(pts[i+1,1]*pts[i,2]))*(pts[i,1]+pts[i+1,1]));
+    y1   = y1 + (1/(6*a))*(((pts[i,1]*pts[i+1,2])-(pts[i+1,1]*pts[i,2]))*(pts[i,2]+pts[i+1,2]));
+  }
+
+  CG = c(x1, y1, pts[1,3]);
+
+  # adjust the incoming points to be so that the origin of the pts is on the plate's center of gravity
+  adj_pts = pts # define the new matrix
+  for (i in 1:4){
+    adj_pts[i,] = pts[i,] - CG
+  }
+
+  # calculate the moment of inertia of the plate
+  x2  = 0;
+  y2  = 0;
+  Ixy = 0;
+  for (i in 1:3){
+    temp = ((adj_pts[i,1]*adj_pts[i+1,2])-(adj_pts[i+1,1]*adj_pts[i,2]))
+    x2   = x2  + (temp*(adj_pts[i,1]^2+(adj_pts[i,1]*adj_pts[i+1,1])+adj_pts[i+1,1]^2));
+    y2   = y2  + (temp*(adj_pts[i,2]^2+(adj_pts[i,2]*adj_pts[i+1,2])+adj_pts[i+1,2]^2));
+    Ixy  = Ixy + (0.5)*(temp*((2*adj_pts[i,1]*adj_pts[i,2])+ adj_pts[i,1]*adj_pts[i+1,2] + adj_pts[i,2]*adj_pts[i+1,1] + (2*adj_pts[i+1,1]*adj_pts[i+1,2])));
+  }
+
+
+  I = matrix(0, nrow = 3, ncol = 3) # define matrix
+  I[1,1] = y2      # Ixx
+  I[2,2] = x2      # Iyy
+  I[3,3] = (x2+y2) # Izz
+  I[1,2] = Ixy     # Ixy
+  I[2,1] = Ixy     # Iyx
+
+  I = (1/12)*rho*t*I
+
+
+  if(desired_prop == "I"){
+    return(I)
+  }
+  if(desired_prop == "CG"){
+    return(CG)
+  }
+}
+
