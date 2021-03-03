bee0d95d2a29447e1842ff3653be1a35f996f714
charvey23 <harveyca@umich.edu>
2021-03-03 15:30:15 -0500

    rename file and final updates

---
+++ transformROM_script.R
@@ -0,0 +1,200 @@
+##### This scripts reads in all range of motion tracks for a specific species.
+##### Resizes down to each individual, subsamples and finally reorients each wing shape
+
+## Load required libraries
+library(tidyverse)
+library(Morpho)
+library(geomorph)
+library(stringr)
+library(gtools)
+
+source('/Users/christinaharvey/Documents/birdmoment/ROM_transformation_functions.R')
+
+### -------------- Read in all morphological measurements ------------
+run_data_path = "/Users/christinaharvey/Dropbox (University of Michigan)/Bird Mass Distribution/rundata/"
+output_path   = "/Users/christinaharvey/Dropbox (University of Michigan)/Bird Mass Distribution/05_transformed_optitrack/"
+
+# all of the non-wing based measurements for all specimens
+dat_bird     = readxl::read_xlsx(paste(run_data_path,"bird_measurements_readytorun.xlsx",sep= ""), sheet = 'Major body parts')
+# all of the wing based measurements for all specimens
+dat_wingspec = readxl::read_xlsx(paste(run_data_path,"bird_measurements_readytorun.xlsx",sep= ""), sheet = 'Within wings')
+
+dat_bird$species = NA
+dat_bird$BirdID  = NA
+for (i in 1:nrow(dat_bird)){
+  if (strsplit(dat_bird$bird_id, "_")[[i]][1] == "COLLI"){
+    dat_bird$species[i] = "col_liv"
+    dat_bird$BirdID[i]  = paste(strsplit(dat_bird$bird_id, "_")[[i]][2],strsplit(dat_bird$bird_id, "_")[[i]][3], sep = "_")
+  }else{
+    dat_bird$species[i] = paste(tolower(strsplit(dat_bird$bird_id, "_")[[i]][1]),strsplit(dat_bird$bird_id, "_")[[i]][2], sep = "_")
+    dat_bird$BirdID[i]  = paste(strsplit(dat_bird$bird_id, "_")[[i]][3],strsplit(dat_bird$bird_id, "_")[[i]][4], sep = "_")
+  }
+}
+
+#-------- Find all file names necessary -------
+working_path  = "/Users/christinaharvey/Dropbox (University of Michigan)/Bird Mass Distribution/03_processed_optitrack/"
+filename.3d   = list.files(path = working_path, pattern = paste('*',".csv",sep=""))
+max_sample_no = 2 # maximum amount of samples to have within one bin
+bin_size      = 2 # #deg x #deg bins that will have the max amount of samples
+
+# create blank data frame
+dat_ID = as.data.frame(matrix(nrow = length(filename.3d), ncol = 4))
+colnames(dat_ID) = c("species","BirdID","TestID","wing_side")
+
+# Save all the identifying information
+for (i in 1:length(filename.3d)){
+  dat_ID$species[i] = paste(tolower(strsplit(filename.3d[i], "_")[[1]][2]),"_",strsplit(filename.3d[i], "_")[[1]][3], sep = "")
+  dat_ID$BirdID[i]  = paste(strsplit(filename.3d[i], "_")[[1]][4],"_",str_sub(strsplit(filename.3d[i], "_")[[1]][5],start = 1, end = -2), sep = "")
+  dat_ID$TestID[i]  = as.numeric(strsplit(filename.3d[i], "_")[[1]][7])
+  dat_ID$wing[i]    = str_sub(strsplit(filename.3d[i], "_")[[1]][5],-1)
+}
+
+no_species = length(unique(dat_ID$species))
+
+## ------------ Loop through each species ---------------
+
+for (i in 1:no_species){
+  curr_species  = unique(dat_ID$species)[i]
+  if(curr_species == "lar_gla"){next} # deal with the gull data seperately
+  # identify which filenames belong to the current species
+  files_to_read = which(dat_ID$species == curr_species)
+  # identify how many body measurements we have for the current species
+  dat_body_curr = subset(dat_bird, use_body == "Y" & species == curr_species)
+
+  # Read in all optitrack data from the given species
+  dat_raw         = read.csv(filename.3d[files_to_read[1]], stringsAsFactors = FALSE,strip.white = TRUE, na.strings = c("NA"))
+  dat_raw$BirdID  = dat_ID$BirdID[files_to_read[1]]
+  dat_raw$TestID  = dat_ID$TestID[files_to_read[1]]
+  dat_raw$wing    = dat_ID$wing[files_to_read[1]]
+  # First few are in mm adjust to cm
+  if (files_to_read[1] < 12){dat_raw[,3:35] <- dat_raw[,3:35]*0.001}
+
+  for (j in 2:length(files_to_read)){
+    tmp = read.csv(filename.3d[files_to_read[j]], stringsAsFactors = FALSE,strip.white = TRUE, na.strings = c("NA"))
+    tmp$BirdID  = dat_ID$BirdID[files_to_read[j]]
+    tmp$TestID  = dat_ID$TestID[files_to_read[j]]
+    tmp$wing    = dat_ID$wing[files_to_read[j]]
+    # First few are in mm adjust to cm
+    if (files_to_read[j] < 12){tmp[,3:35] <- tmp[,3:35]*0.001}
+    # The storm petrel had one wing track all points but not the other
+    if (ncol(tmp) > ncol(dat_raw)){
+      dat_raw$humerus_leading_position_x = dat_raw$humerus_position_x
+      dat_raw$humerus_leading_position_y = dat_raw$humerus_position_y
+      dat_raw$humerus_leading_position_z = dat_raw$humerus_position_z
+
+      dat_raw$wrist_leading_position_x = dat_raw$wrist_position_x
+      dat_raw$wrist_leading_position_y = dat_raw$wrist_position_y
+      dat_raw$wrist_leading_position_z = dat_raw$wrist_position_z
+
+      dat_raw$cmc_leading_position_x = dat_raw$cmc_position_x
+      dat_raw$cmc_leading_position_y = dat_raw$cmc_position_y
+      dat_raw$cmc_leading_position_z = dat_raw$cmc_position_z
+    }
+    dat_raw = smartbind(dat_raw, tmp)
+  }
+
+  dat_raw$species = curr_species
+
+  ### ---------------- Rename the columns appropriately -----------------
+  dat_raw <- as_tibble(dat_raw)
+  dat_raw <- rename(dat_raw,
+                    FrameID = frame,
+                    pt1_X = humerus_position_x,          pt1_Y = humerus_position_y,          pt1_Z = humerus_position_z,
+                    pt2_X = elbow_position_x,            pt2_Y = elbow_position_y,            pt2_Z = elbow_position_z,
+                    pt3_X = wrist_position_x,            pt3_Y = wrist_position_y,            pt3_Z = wrist_position_z,
+                    pt4_X = cmc_position_x,              pt4_Y = cmc_position_y,              pt4_Z = cmc_position_z,
+                    pt8_X = p10_position_x,              pt8_Y = p10_position_y,              pt8_Z = p10_position_z,
+                    pt9_X = p7_position_x,               pt9_Y = p7_position_y,               pt9_Z = p7_position_z,
+                    pt10_X = s1_position_x,              pt10_Y = s1_position_y,              pt10_Z = s1_position_z,
+                    pt11_X = s10_position_x,             pt11_Y = s10_position_y,             pt11_Z = s10_position_z)
+  # For wings where the leading positions were too close to the joints to differentiate set the joint positions as the edge points as well
+  if (ncol(dat_raw) == 39){
+    dat_raw <- rename(dat_raw,
+                      pt6_X = wrist_leading_position_x,    pt6_Y = wrist_leading_position_y,    pt6_Z = wrist_leading_position_z,
+                      pt7_X = cmc_leading_position_x,      pt7_Y = cmc_leading_position_y,      pt7_Z = cmc_leading_position_z,
+                      pt12_X = humerus_leading_position_x, pt12_Y = humerus_leading_position_y, pt12_Z = humerus_leading_position_z)
+    if(i == 1){
+      col_dat_raw = colnames(dat_raw) # save the order of the row names
+    }
+  }else{
+    dat_raw$pt12_X = dat_raw$pt1_X
+    dat_raw$pt12_Y = dat_raw$pt1_Y
+    dat_raw$pt12_Z = dat_raw$pt1_Z
+
+    dat_raw$pt6_X = dat_raw$pt3_X
+    dat_raw$pt6_Y = dat_raw$pt3_Y
+    dat_raw$pt6_Z = dat_raw$pt3_Z
+
+    dat_raw$pt7_X = dat_raw$pt4_X
+    dat_raw$pt7_Y = dat_raw$pt4_Y
+    dat_raw$pt7_Z = dat_raw$pt4_Z
+
+  }
+
+  dat_raw       = as.data.frame(dat_raw[complete.cases(dat_raw[,3:ncol(dat_raw)]),])  #Remove NaN rows
+  dat_raw       = dat_raw[,c(col_dat_raw)] # re-order accordingly will work as long as the first species tracks all points
+
+  # ---- Calculate the elbow and wrist angle for each frame
+  dat_raw$elbow <- NA
+  dat_raw$manus <- NA
+  #-- Loop through every row to compute elbow and manus angle
+  for (j in 1:nrow(dat_raw)){
+    dat_raw$elbow[j]     <- jointangles(dat_raw$pt1_X[j],dat_raw$pt1_Y[j],dat_raw$pt1_Z[j],
+                                        dat_raw$pt2_X[j],dat_raw$pt2_Y[j],dat_raw$pt2_Z[j],
+                                        dat_raw$pt3_X[j],dat_raw$pt3_Y[j],dat_raw$pt3_Z[j])
+    dat_raw$manus[j]     <- jointangles(dat_raw$pt2_X[j],dat_raw$pt2_Y[j],dat_raw$pt2_Z[j],
+                                        dat_raw$pt3_X[j],dat_raw$pt3_Y[j],dat_raw$pt3_Z[j],
+                                        dat_raw$pt4_X[j],dat_raw$pt4_Y[j],dat_raw$pt4_Z[j])
+
+  }
+  # Bin the data
+  dat_raw$elbow_round = bin_size*round(dat_raw$elbow/bin_size)
+  dat_raw$manus_round = bin_size*round(dat_raw$manus/bin_size)
+
+  dat_raw$BirdID_FrameSpec = dat_raw$BirdID
+
+
+  ## ---- Loop through each body and subsample, resize, reorient the wings appropriately ------
+
+  for (ind in 1:nrow(dat_body_curr)){
+
+    curr_BirdID = dat_body_curr$BirdID[ind] # save the current BirdID
+
+    col_char = c("species","BirdID","TestID","BirdID_FrameSpec","FrameID","time_sec","wing","elbow","manus","elbow_round","manus_round")
+    col_all  = c(col_char, colnames(dat_raw[,3:35]))
+
+    #### ------- Resize the applicable wings --------
+    for (j in 1:nrow(dat_body_curr)){
+      if (dat_body_curr$BirdID[j] != curr_BirdID){
+        tmp = resize_to(specimen_to_adjust = subset(dat_raw, species == curr_species & BirdID == dat_body_curr$BirdID[j]),
+                        target_specimen = subset(dat_raw, species == curr_species & BirdID == curr_BirdID),
+                        char_colnames = col_char)
+        colnames(tmp) = col_all
+      }else {
+        tmp = subset(dat_raw, species == curr_species & BirdID == curr_BirdID)
+        tmp = tmp[,col_all]}
+
+      if(j == 1){
+        dat_resized = tmp} else{
+        dat_resized = rbind(dat_resized,tmp)}
+    }
+    # Adjust the BirdID to reflect the current bird - BirdID_FrameSpec indicates which bird the frame originally came from
+    dat_resized$BirdID = curr_BirdID
+
+    #### ------- Subsample the current data frame --------
+    # only keep the unique values
+    dat_keep <- dat_resized[!allDup(dat_resized[,c("elbow_round","manus_round")]),]
+    # save all duplicated rows so that we can randomly select one value for each bin and add back
+    dat_dup  <- dat_resized[allDup(dat_resized[,c("elbow_round","manus_round")]),]
+
+    dat_subsample = subsample_frames(dat_keep,dat_dup,curr_BirdID,max_sample_no)
+
+    #### ------- Reorient the wings --------
+    dat_complete = reorient_wings(dat_subsample)
+
+    # ------------------------------ Save data
+    filename_new <- paste(output_path,format(Sys.Date(), "%Y_%m_%d"),"_",curr_species,"_",curr_BirdID,"_transformed.csv",sep = "")
+    write.csv(dat_complete,filename_new)
+  } # end of the specimen loop
+} # end of the species loop
+
---
+++ transformROM_supportingfunctions.R
@@ -0,0 +1,331 @@
+
+
+
+## This code reads in three points in 3D and computs the angle where the second point is the central point
+## Requires standard deviation on the calculation as well.
+
+## To calculate the reflex angle this must be checked for each specimen independently due to the orientation and truncation errors
+
+## Written by: Christina Harvey
+## Date: Feb 2020
+
+#' Title
+#'
+#' @param x1 1st component of the 1st point
+#' @param y1 2nd component of the 1st point
+#' @param z1 3rd component of the 1st point
+#' @param x2 1st component of the 2nd point
+#' @param y2 2nd component of the 2nd point
+#' @param z2 3rd component of the 2nd point
+#' @param x3 1st component of the 3rd point
+#' @param y3 2nd component of the 3rd point
+#' @param z3 3rd component of the 3rd point
+#'
+#' @return the interior angle between the input three points
+#' @export
+#'
+#' @examples
+jointangles <- function(x1,y1,z1,x2,y2,z2,x3,y3,z3){
+
+  #pt 2 is the central point to compute the angle around
+  # Calculate the vectors
+  u1 = c(x1,y1,z1)-c(x2,y2,z2)
+  v1 = c(x3,y3,z3)-c(x2,y2,z2)
+
+  interior = dot(u1,v1)/(Norm(u1)*Norm(v1))
+
+  angle = acos(interior)*180/pi
+
+  return(angle)
+}
+
+
+allDup <- function (value) # found online by jholtman at gmail.com
+{duplicated(value) | duplicated(value, fromLast = TRUE)}
+
+
+
+
+################################ Custom functions ##############################
+## Just source these and move on to the next section
+
+## This function scales wings up or down based on a scale factor
+scale_the_wing <- function(specimens, ## MUST BE $rotated component of procSym output
+                           scale_factor ## numeric scaling factor
+) {
+
+  ## figure out dimensionality of array
+  n <- dim(specimens)[3]
+  k <- dim(specimens)[1]
+  m <- dim(specimens)[2]
+  x <- specimens
+
+  resizelist <-  array(numeric(),c(k, m ,n))
+  for (i in 1:n) {
+    resizelist[,,i] <-
+      x[,,i] * scale_factor
+  }
+
+  ## export as a 3D array
+  return(resizelist)
+}
+
+## This function re-sizes the optitrack data for a specimen (specimen_to_adjust)
+## based on the size of a second specimen (target_specimen)
+resize_to <- function(specimen_to_adjust,target_specimen,char_colnames,tol = 5e-3){
+
+  ## make names shorter for ease of coding later
+  x1 <- specimen_to_adjust %>%
+    select(-char_colnames) %>%
+    drop_na() %>%
+    arrayspecs(p = 11, k = 3) %>%
+    procSym(scale = FALSE, CSinit=FALSE)
+  x2 <- target_specimen %>%
+    select(-char_colnames) %>%
+    drop_na() %>%
+    arrayspecs(p = 11, k = 3) %>%
+    procSym(scale = FALSE, CSinit=FALSE)
+
+  ## get into 2d array
+  x1_two_d <- two.d.array(x1$rotated)
+  x2_two_d <- two.d.array(x2$rotated)
+
+  ## get the length of the humerus in each specimen
+  x1_humerus_lengths <-
+    calc_dist(x1_two_d[,7:12])
+  x2_humerus_lengths <-
+    calc_dist(x2_two_d[,7:12])
+
+  ## set the scale_factor based on the means of the humerus lengths
+  scale_factor <- mean(x2_humerus_lengths)/mean(x1_humerus_lengths)
+
+  ## rescale the wing according to the scale factor
+  rescaled <- scale_the_wing(x1$rotated, scale_factor = scale_factor)
+
+  rescaled_two_d <- two.d.array(rescaled)
+
+  ## check humerus lengths
+  rescaled_hums <-
+    calc_dist(rescaled_two_d[,7:12])
+
+  hum_diffs <- mean(rescaled_hums) - mean(x2_humerus_lengths)
+
+  if (abs(hum_diffs) > tol) {
+    stop("shit happened. give up.")
+  }
+
+  ## isolate the frames and times
+  spec_framtimes <-
+    specimen_to_adjust %>%
+    drop_na() %>%
+    select(char_colnames)
+
+  ## paste it back together
+  export_dat <-
+    data.frame(spec_framtimes,
+               rescaled_two_d)
+  colnames(export_dat) <- colnames(specimen_to_adjust)
+
+  ## export
+  return(export_dat)
+
+}
+
+################################## calc_dist ###################################
+# this function calculates the 3D vector between two points
+# input: a dataframe with 6 columns, first 3 columns are coordinates for one point
+# last 3 columns are coordinates for another point
+# output: a numeric value
+calc_dist <- function(pair) {
+
+  dx = pair[,1]-pair[,4]
+  dy = pair[,2]-pair[,5]
+  dz = pair[,3]-pair[,6]
+  dist = sqrt(dx^2+dy^2+dz^2)
+
+  return(dist)
+}
+
+
+
+
+subsample_frames <- function(dat_keep,dat_dup,curr_BirdID,max_sample_no){
+
+  dat_dup_uni   <- unique(dat_dup[,c("elbow_round","manus_round")])
+  # Loop through each duplicated values and save one random sample
+  for (j in 1:nrow(dat_dup_uni)){
+    # limit to the current unique bin for the specific individual we are looking at
+    tmp = subset(dat_dup, elbow_round == dat_dup_uni$elbow_round[j] &
+                   manus_round == dat_dup_uni$manus_round[j] &
+                   BirdID == curr_BirdID)
+
+    # if there are more frames than desired samples than choose at random
+    if (nrow(tmp) > max_sample_no){
+      tmp = subset(tmp, FrameID %in% sample(tmp$FrameID, max_sample_no))
+    }
+    # if there are not enough frames than desired samples than keep those but add in randomly sampled frames from other individuals
+    if (nrow(tmp) < max_sample_no){
+      tmp_oth = subset(dat_dup, elbow_round == dat_dup_uni$elbow_round[j] &
+                         manus_round == dat_dup_uni$manus_round[j] &
+                         BirdID != curr_BirdID)
+      row_rand = sample(tmp_oth[,c("FrameID","BirdID")], max_sample_no-nrow(tmp))
+      tmp_oth  = subset(tmp_oth, FrameID %in% sample(tmp_oth$FrameID, max_sample_no-nrow(tmp)))
+      tmp      = rbind(tmp,tmp_oth)
+    }
+
+    # Save the selected rows
+    if (j == 1){
+      dat_subsample <- rbind(dat_keep,tmp)
+    }
+    dat_subsample <- rbind(dat_subsample,tmp)
+  } # end of the loop through joint angle bins
+
+  return(dat_subsample)
+}
+
+
+
+# ---- This code is to be used with the LLT takes raw digitized points and prepares them for testing
+#When re-running for new data double check that the transformations are working correctly.
+
+# CAUTION: This code is now set up to orient wings as follows:
+#          1. Pt3 (Wrist) will be inline with Pt1 (humeral head) along the y axis
+#          2. Pt3 (Wrist) will be inline with Pt1 (humeral head) along the z axis
+#          3. Pt3 (Wrist) will be inline with Pt10 (S1) along the x axis
+
+# Created: Christina Harvey
+# Last updated: 16-Dec-2020
+
+reorient_wings<- function(dat_subsample){
+  # ----------------- Define set variables ------------
+  point_list <- c("pt1","pt2","pt3","pt4","pt6","pt7","pt8","pt9","pt10","pt11","pt12")
+  dim_list   <- c("X","Y","Z")
+  xaxis <- c(1, 0, 0)
+  yaxis <- c(0, 1, 0)
+  zaxis <- c(0, 0, 1)
+
+  ## ------- Reorient Wings ---------
+  dat_clean <- dat_subsample
+  #------------------------------- Step 1 -------------------------------
+  ##### error doesn't adjust all points ######
+  #Make pt 1 on humerus the beginning location
+  for (j in 2:11){
+    for (k in 1:3){
+      col_name = paste(point_list[j],dim_list[k],sep = "_")
+      pt1_name = paste(point_list[1],dim_list[k],sep = "_")
+      dat_clean[,col_name] <- dat_clean[,col_name] - dat_clean[,pt1_name]
+    }
+  }
+  dat_clean[,c("pt1_X","pt1_Y","pt1_Z")] <- 0
+
+  #-------------------------------  Step 2 -------------------------------
+  # Set pt 3 in line with Pt 1 along the wingspan
+  #Calculate angle between axis that runs along wing length and pt 3 (rotate about z)
+  #Project onto xaxis & make negative if j1 > 0:
+  i1 = dat_clean$pt3_X
+  j1 = dat_clean$pt3_Y
+
+  dot_xaxis = i1
+  vec_norm  = sqrt(i1^2 + j1^2)
+  interior  = dot_xaxis/vec_norm
+  thetaz    = acos(interior) + pi / 2
+  thetaz[which(j1 > 0)] = -acos(interior[which(j1 > 0)]) + pi / 2
+
+  # Rotate about the z axis
+  tmp <- dat_clean
+  for (j in 1:11){
+    for (k in 1:3){
+      col_name = paste(point_list[j],dim_list[k],sep = "_")
+      if (k == 1){
+        dat_clean[,col_name] = cos(thetaz) * tmp[,col_name] - sin(thetaz) * tmp[,paste(point_list[j],dim_list[2],sep = "_")]
+      } else{
+        if(k == 2){
+          dat_clean[,col_name] = sin(thetaz) * tmp[,paste(point_list[j],dim_list[1],sep = "_")] + cos(thetaz) * tmp[,col_name]
+        }
+      }
+    }
+  }
+  # CAUTION: Verify that Pt3X=x_adj1=0 after this step - View(subset(pts,Pt.No == 3))
+
+  #------------------------------- Step 3 -------------------------------
+  # Set pt 3 in line with Pt 1 looking down the length of wing
+  # Calculate angle between x axis and pt 3 (rotate about y)
+  # Project onto xaxis & make negative if k2 > 0
+  j2 = dat_clean$pt3_Y
+  k2 = dat_clean$pt3_Z
+
+  dot_yaxis = j2
+  vec_norm  = sqrt(j2^2 + k2^2)
+  interior = dot_yaxis/vec_norm
+
+  thetax = acos(interior)
+  thetax[which(k2 > 0)] = -acos(interior[which(k2 > 0)])
+
+  # Rotate about the x axis
+  tmp <- dat_clean
+  for (j in 1:11){
+    for (k in 1:3){
+      col_name = paste(point_list[j],dim_list[k],sep = "_")
+      if (k == 2){
+        dat_clean[,col_name] = cos(thetax) * tmp[,col_name] - sin(thetax) * tmp[,paste(point_list[j],dim_list[3],sep = "_")]
+      } else{
+        if(k == 3){
+          dat_clean[,col_name] = sin(thetax) * tmp[,paste(point_list[j],dim_list[2],sep = "_")] + cos(thetax) * tmp[,col_name]
+        }
+      }
+    }
+  }
+
+  # CAUTION: Verify that Pt3Z=z_adj2=0 after this step - View(subset(pts,Pt.No == 3))
+
+  #------------------------------- Step 4 -------------------------------
+  # Set pt 10 in line with Pt 3 looking from back of wing
+  # Calculate angle between x axis and pt 10 (rotate about y)
+  # Project onto yaxis & make negative if k2 > 0
+  i3 = dat_clean$pt10_X
+  k3 = dat_clean$pt10_Z
+
+  dot_xaxis = i3
+  vec_norm  = sqrt(i3^2 + k3^2)
+  interior  = dot_xaxis/vec_norm
+
+  thetay = -acos(interior)
+  thetay[which(k3 > 0)] = acos(interior[which(k3 > 0)])
+  #thetay[which(dat_clean$pt2_X < 0)] = thetay[which(dat_clean$pt2_X < 0)] + pi
+
+  # Rotate about the y axis
+  tmp <- dat_clean
+  for (j in 1:11){
+    for (k in 1:3){
+      col_name = paste(point_list[j],dim_list[k],sep = "_")
+      if (k == 1){
+        dat_clean[,col_name] = cos(thetay) * tmp[,col_name] + sin(thetay) * tmp[,paste(point_list[j],dim_list[3],sep = "_")]
+      } else{
+        if(k == 3){
+          dat_clean[,col_name] = -sin(thetay) * tmp[,paste(point_list[j],dim_list[1],sep = "_")] + cos(thetay) * tmp[,col_name]
+        }
+      }
+    }
+  }
+  # CAUTION: Verify that Pt10Z=z_adj3=0 after this step - View(subset(pts,Pt.No == 10))
+
+  #------------------------------- Step 5 -------------------------------
+  for (j in 1:11) {
+    col_name = paste(point_list[j], dim_list[1], sep = "_")
+    dat_clean[, col_name] <- -dat_clean[, col_name]
+
+    col_name = paste(point_list[j], dim_list[3], sep = "_")
+    dat_clean[which(dat_clean$wing == "R"), col_name] <- -dat_clean[which(dat_clean$wing == "R"), col_name]
+  }
+
+  dat_clean$S_proj <- NA
+  for (i in 1:nrow(dat_clean)){
+    # Calculate the projected area for each wing - this is the correct order because X is negative and Y is positive
+    x_vertices = c(dat_clean$pt6_X[i],dat_clean$pt7_X[i],dat_clean$pt8_X[i],dat_clean$pt9_X[i],dat_clean$pt10_X[i],dat_clean$pt11_X[i],dat_clean$pt12_X[i])
+    y_vertices = c(dat_clean$pt6_Y[i],dat_clean$pt7_Y[i],dat_clean$pt8_Y[i],dat_clean$pt9_Y[i],dat_clean$pt10_Y[i],dat_clean$pt11_Y[i],dat_clean$pt12_Y[i])
+    dat_clean$S_proj[i] <- polyarea(x_vertices, y_vertices)
+  }
+
+  return(dat_clean)
+
+}
